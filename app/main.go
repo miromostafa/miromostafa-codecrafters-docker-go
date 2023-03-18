d2c38bb295cfd597e13a8095b0e8da623aa7071e
amroakmal <amr.akmal@incorta.com>
2023-03-18 12:33:31 +0200

    Stage 2

--- app/main.go
+++ app/main.go
@@ -18,9 +18,15 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 	
 	cmd := exec.Command(command, args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
+		// fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
 	
